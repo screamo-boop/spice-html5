404b05f61020d38acfd9c87ee69b82b8da9b4d42
Danil Lavrov <lavrov-ds@rosenergoatom.ru>
2025-01-24 15:03:23 +0500

    Hide cursor for simulation

--- src/cursor.js
+++ src/cursor.js
@@ -95,10 +95,10 @@ class SpiceCursorConn extends SpiceConn {
         this.set_cursor(cursor_set.cursor);
         return true;
     }
-// FIXME: Temporary disabled cursor hide for oldest operating systems
+
     hideCursor() {
         DEBUG > 1 && console.log("SpiceMsgCursorHide");
-        this.setCursorStyle("auto");
+        this.setCursorStyle("none");
         return true;
     }
 
